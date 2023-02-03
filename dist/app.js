a6ae22f59010926c99fef9647c6558d2d001a27a
Kertich <kipsangkertich@gmail.com>
2023-02-03 16:36:32 +0300

    form validation

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,13 @@ const plus = document.getElementById("plus");
 const cancelBtn = document.getElementById("cancelBtn");
 const UI1 = document.querySelector(".UI1");
 const UI2 = document.querySelector(".UI2");
+const taskName = document.getElementById("taskName");
+const taskImage = document.getElementById("taskImage");
+const startDate = document.getElementById("startDate");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const form = document.getElementById("form");
+const pleaseAdd = document.getElementById("pleaseAdd");
+const TaskBtn = document.getElementById("TaskBtn");
 plus.addEventListener('click', () => {
     UI1.style.display = 'none';
     UI2.style.display = 'block';
@@ -10,4 +17,28 @@ plus.addEventListener('click', () => {
 cancelBtn.addEventListener('click', () => {
     UI1.style.display = 'block';
     UI2.style.display = 'none';
+});
+const showMessage = (message) => {
+    const messageDiv = document.createElement("div");
+    messageDiv.innerText = message;
+    // messageDiv.style.backgroundColor = "blue";
+    // messageDiv.style.color = "black";
+    // messageDiv.style.padding = "10px";
+    // messageDiv.style.position = "fixed";
+    // messageDiv.style.bottom = "10px";
+    // messageDiv.style.right = "10px";
+    // messageDiv.style.zIndex = "9999";
+    pleaseAdd.appendChild(messageDiv);
+    setTimeout(() => {
+        pleaseAdd.removeChild(messageDiv);
+    }, 5000);
+    //console.log(pleaseAdd);
+};
+TaskBtn.addEventListener("click", function () {
+    // console.log("Button clicked.");
+    // Example usage:
+    if (taskName.value === "") {
+        // console.log("hh");
+        showMessage("Validation failed. Please check your inputs and try again.");
+    }
 });
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
-const plus = document.getElementById("plus") as HTMLAnchorElement
+const plus = document.getElementById("plus") as HTMLElement
 const cancelBtn = document.getElementById("cancelBtn") as HTMLElement
-const UI1 = document.querySelector(".UI1") as HTMLAreaElement
-const UI2 = document.querySelector(".UI2") as HTMLAnchorElement
+const UI1 = document.querySelector(".UI1") as HTMLElement
+const UI2 = document.querySelector(".UI2") as HTMLElement
+const taskName = document.getElementById("taskName") as HTMLInputElement
+const taskImage = document.getElementById("taskImage") as HTMLInputElement
+const startDate = document.getElementById("startDate") as HTMLElement
+const addTaskBtn = document.getElementById("addTaskBtn") as HTMLButtonElement
+const form = document.getElementById("form") as HTMLElement
+const pleaseAdd = document.getElementById("pleaseAdd") as HTMLDivElement
+const TaskBtn = document.getElementById("TaskBtn") as HTMLAnchorElement
+
 
 plus.addEventListener('click',()=>{
     UI1.style.display = 'none'
@@ -12,3 +20,41 @@ cancelBtn.addEventListener('click',()=>{
     UI1.style.display = 'block'
     UI2.style.display = 'none'
 })
+
+const showMessage = (message: string) => {
+    const messageDiv = document.createElement("div") as HTMLDivElement;
+    messageDiv.innerText = message;
+    // messageDiv.style.backgroundColor = "blue";
+    // messageDiv.style.color = "black";
+    // messageDiv.style.padding = "10px";
+    // messageDiv.style.position = "fixed";
+    // messageDiv.style.bottom = "10px";
+    // messageDiv.style.right = "10px";
+    // messageDiv.style.zIndex = "9999";
+    pleaseAdd.appendChild(messageDiv);
+    setTimeout(() => {
+      pleaseAdd.removeChild(messageDiv);
+    }, 5000);
+    //console.log(pleaseAdd);
+    
+    
+  };
+  
+  
+  
+ 
+
+TaskBtn.addEventListener("click", function() {
+ // console.log("Button clicked.");
+ // Example usage:
+
+ if (
+    taskName.value && taskImage.value === ""
+    
+) {
+   // console.log("hh");
+    
+showMessage("Validation failed. Please check your inputs and try again.");
+}
+
+});
