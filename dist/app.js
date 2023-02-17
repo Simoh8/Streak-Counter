72552a6c60e2000715e5d014943d8cf47bed41b3
simon muturi <simomutu@gmail.com>
2023-02-17 17:11:49 +0300

    a fixed version

--- dist/app.js
+++ dist/app.js
@@ -10,6 +10,9 @@ const addTaskBtn = document.getElementById("addTaskBtn");
 const form = document.getElementById("form");
 const pleaseAdd = document.getElementById("pleaseAdd");
 const TaskBtn = document.getElementById("TaskBtn");
+const UI1bottom = document.querySelector(".UI1bottom");
+const ItemsBlock = document.querySelector(".noTask");
+const Items = [];
 plus.addEventListener('click', () => {
     UI1.style.display = 'none';
     UI2.style.display = 'block';
@@ -18,27 +21,21 @@ cancelBtn.addEventListener('click', () => {
     UI1.style.display = 'block';
     UI2.style.display = 'none';
 });
-const showMessage = (message) => {
-    const messageDiv = document.createElement("div");
-    messageDiv.innerText = message;
-    // messageDiv.style.backgroundColor = "blue";
-    // messageDiv.style.color = "black";
-    // messageDiv.style.padding = "10px";
-    // messageDiv.style.position = "fixed";
-    // messageDiv.style.bottom = "10px";
-    // messageDiv.style.right = "10px";
-    // messageDiv.style.zIndex = "9999";
-    pleaseAdd.appendChild(messageDiv);
-    setTimeout(() => {
-        pleaseAdd.removeChild(messageDiv);
-    }, 5000);
-    //console.log(pleaseAdd);
-};
-TaskBtn.addEventListener("click", function () {
-    // console.log("Button clicked.");
-    // Example usage:
-    if (taskName.value === "") {
-        // console.log("hh");
-        showMessage("Validation failed. Please check your inputs and try again.");
-    }
+TaskBtn.addEventListener('click', () => {
+    const taskname = taskName.value;
+    const taskimage = taskImage.value;
+    const start = startDate.value;
+    const singlestreak = {
+        taskimage,
+        taskname,
+        start
+    };
+    Items.push(singlestreak);
+    Items.map((item) => {
+        console.log(item);
+        let div = document.createElement("div");
+        UI1bottom.innerHTML = "wdfghjk";
+        div.value = item;
+        UI1bottom.appendChild(item);
+    });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,47 @@
-const plus = document.getElementById("plus") as HTMLElement
-const cancelBtn = document.getElementById("cancelBtn") as HTMLElement
-const UI1 = document.querySelector(".UI1") as HTMLElement
-const UI2 = document.querySelector(".UI2") as HTMLElement
+const plus = document.getElementById("plus") as HTMLButtonElement
+const cancelBtn = document.getElementById("cancelBtn") as HTMLButtonElement
+const UI1 = document.querySelector(".UI1") as HTMLDivElement
+const UI2 = document.querySelector(".UI2") as HTMLDivElement
 const taskName = document.getElementById("taskName") as HTMLInputElement
 const taskImage = document.getElementById("taskImage") as HTMLInputElement
-const startDate = document.getElementById("startDate") as HTMLElement
+const startDate = document.getElementById("startDate") as HTMLInputElement
 const addTaskBtn = document.getElementById("addTaskBtn") as HTMLButtonElement
-const form = document.getElementById("form") as HTMLElement
+const form = document.getElementById("form") as HTMLFormElement
 const pleaseAdd = document.getElementById("pleaseAdd") as HTMLDivElement
-const TaskBtn = document.getElementById("TaskBtn") as HTMLAnchorElement
+const TaskBtn = document.getElementById("TaskBtn") as HTMLButtonElement
+const UI1bottom = document.querySelector(".UI1bottom") as HTMLDivElement
+const ItemsBlock = document.querySelector(".noTask") as HTMLDivElement
+const Items: object[] = []
 
-
-plus.addEventListener('click',()=>{
-    UI1.style.display = 'none'
-    UI2.style.display = 'block'
+plus.addEventListener('click', () => {
+  UI1.style.display = 'none'
+  UI2.style.display = 'block'
 })
 
-cancelBtn.addEventListener('click',()=>{
-    UI1.style.display = 'block'
-    UI2.style.display = 'none'
+cancelBtn.addEventListener('click', () => {
+  UI1.style.display = 'block'
+  UI2.style.display = 'none'
 })
 
-const showMessage = (message: string) => {
-    const messageDiv = document.createElement("div") as HTMLDivElement;
-    messageDiv.innerText = message;
-    // messageDiv.style.backgroundColor = "blue";
-    // messageDiv.style.color = "black";
-    // messageDiv.style.padding = "10px";
-    // messageDiv.style.position = "fixed";
-    // messageDiv.style.bottom = "10px";
-    // messageDiv.style.right = "10px";
-    // messageDiv.style.zIndex = "9999";
-    pleaseAdd.appendChild(messageDiv);
-    setTimeout(() => {
-      pleaseAdd.removeChild(messageDiv);
-    }, 5000);
-    //console.log(pleaseAdd);
-    
-    
-  };
-  
-  
-  
- 
+TaskBtn.addEventListener('click', () => {
+  const taskname = taskName.value
+  const taskimage = taskImage.value
+  const start = startDate.value
 
-TaskBtn.addEventListener("click", function() {
- // console.log("Button clicked.");
- // Example usage:
+  const singlestreak = {
+    taskimage,
+    taskname,
+    start
+  }
 
- if (
-    taskName.value && taskImage.value === ""
-    
-) {
-   // console.log("hh");
-    
-showMessage("Validation failed. Please check your inputs and try again.");
-}
+  Items.push(singlestreak)
+  Items.map((item) => {
+    console.log(item);
 
-});
+    let div = document.createElement("div")
+    UI1bottom.innerHTML="wdfghjk"
+    div.value = item
+      UI1bottom.appendChild(item)
+    })
+
+})
